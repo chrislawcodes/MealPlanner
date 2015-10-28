84bbc30c43978f98c86df2734bca16d5cbda884e
chrislawcodes <chrislaw@gmail.com>
2015-10-27 19:08:19 -0700

    in the midst of hooking up API

--- BrowseRecipesViewController.swift
+++ BrowseRecipesViewController.swift
@@ -13,14 +13,15 @@ class BrowseRecipesViewController: UIViewController, UITableViewDelegate, UITabl
     @IBOutlet weak var browseTableView: UITableView!
     
     var recipenames: [String]!
+    var recipes: [NSDictionary]!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         browseTableView.delegate = self
         browseTableView.dataSource = self
-        
-        recipenames = ["Shrimp", "Chicken", "Beef", "Salad"]
+       
+        recipes = []
         
         let url = NSURL(string: "http://food2fork.com/api/search?key=7e1cd92e04fda52d2520cfc3046b9f4b&q=shredded%20chicken")!
         let request = NSURLRequest(URL: url)
@@ -28,7 +29,14 @@ class BrowseRecipesViewController: UIViewController, UITableViewDelegate, UITabl
         NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue())
             { (response:NSURLResponse?, data: NSData?, error: NSError?) -> Void in
                 
-                let json = try! NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.AllowFragments)
+                let json = try! NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.AllowFragments) as! NSDictionary
+                
+                print(json)
+                
+                self.recipes = json["recipes"] as! [NSDictionary]
+                self.browseTableView.reloadData()
+    
+                
         }
     }
 
@@ -41,8 +49,8 @@ class BrowseRecipesViewController: UIViewController, UITableViewDelegate, UITabl
     }
     
 
-    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return recipenames.count
+    func tableView(tableView: UITableView, numberOfRowsInSection section:Int) -> Int {
+        return recipes.count
     
     }
 
@@ -51,6 +59,7 @@ class BrowseRecipesViewController: UIViewController, UITableViewDelegate, UITabl
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
     
         let cell = browseTableView.dequeueReusableCellWithIdentifier("ResultCell")! as! ResultCell
+
     
     
     cell.recipenameLabel.text = recipenames[indexPath.row]
