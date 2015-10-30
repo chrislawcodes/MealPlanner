b1d40f53fbe3601657e0f001c0cb1e21fc82bef1
chrislawcodes <chrislaw@gmail.com>
2015-10-29 23:33:38 -0700

    picker

--- IndividualRecipeViewController.swift
+++ IndividualRecipeViewController.swift
@@ -9,20 +9,32 @@
 import UIKit
 import AFNetworking
 
-class IndividualRecipeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class IndividualRecipeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource,  UIPickerViewDataSource,UIPickerViewDelegate {
     
     @IBOutlet weak var ingredientTableView: UITableView!
     @IBOutlet weak var recipenameLabel: UILabel!
     @IBOutlet weak var recipeImageView: UIImageView!
+    @IBOutlet weak var displayLabel: UILabel!
+    @IBOutlet weak var assignPicker: UIPickerView!
+    @IBOutlet weak var mealdatePicker: UIView!
     
     var recipe: NSDictionary!
     var recipeID: String!
     var urlString: String!
     var ingredientlistArray: [String]!
-
+    
+    let pickerData = [
+        ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
+        ["Breakfast","Lunch","Dinner"],
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        mealdatePicker.alpha = 0
+        
+        assignPicker.dataSource = self
+        assignPicker.delegate = self
         
         ingredientTableView.delegate = self
         ingredientTableView.dataSource = self
@@ -40,7 +52,7 @@ class IndividualRecipeViewController: UIViewController, UITableViewDelegate, UIT
                 
                 print(json)
                 self.recipe = json["recipe"] as! NSDictionary
-               
+                
                 self.recipenameLabel.text = self.recipe["title"] as! String
                 self.urlString = self.recipe["image_url"] as! String
                 self.recipeImageView.setImageWithURL(NSURL(string: self.urlString)!)
@@ -48,14 +60,14 @@ class IndividualRecipeViewController: UIViewController, UITableViewDelegate, UIT
                 self.ingredientTableView.reloadData()
                 
         }
-       
+        
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     func tableView(tableView: UITableView, numberOfRowsInSection section:Int) -> Int {
         return ingredientlistArray.count
     }
@@ -69,16 +81,32 @@ class IndividualRecipeViewController: UIViewController, UITableViewDelegate, UIT
         return cell
         
     }
-
     
-    /*
-    // MARK: - Navigation
-    
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-    // Get the new view controller using segue.destinationViewController.
-    // Pass the selected object to the new view controller.
+    //Picker Code
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return pickerData.count
     }
-    */
     
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData[component].count
+    }
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String! {
+        return pickerData[component][row]
+    }
+    
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        updateLabel()
+    }
+    
+    func updateLabel(){
+        let date = pickerData[0][assignPicker.selectedRowInComponent(0)]
+        let meal = pickerData[1][assignPicker.selectedRowInComponent(1)]
+        displayLabel.text = date + " for " + meal
+    }
+    
+    @IBAction func assignmealButton(sender: UIButton) {
+        mealdatePicker.alpha = 1
+    }
+
 }
